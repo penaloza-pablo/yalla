738254cb329405de33a8ab72d45876679f86a30d
Pablo Peñaloza M. <pablo.penaloza@Mac-mini-de-Pablo.local>
2026-01-16 17:37:38 +0100

    Inventory + alerts + beta chatbot

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from '@aws-amplify/ui-react'
 import './index.css'
 import App from './App.tsx'
 
+Amplify.configure(outputs)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </StrictMode>,
 )
